bed500bf9af1a9fd0c43dec4077fbdf5000a06c7
mohamed belhadj <belmoh70@gmail.com>
2025-04-05 23:49:37 +0200

    front-url

--- trip_planner_api/settings.py
+++ trip_planner_api/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
 # ========================
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "https://trip-planner-front-five.vercel.app/", 
+    "https://trip-planner-front-five.vercel.app", 
 ]
 
 CSRF_TRUSTED_ORIGINS = CORS_ALLOWED_ORIGINS.copy()
