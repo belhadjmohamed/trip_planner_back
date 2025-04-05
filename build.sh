492135ece5a91f89fbdfad9dc83318133459ff07
mohamed belhadj <belmoh70@gmail.com>
2025-04-05 22:05:37 +0200

    fix-build

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Install dependencies
-pip install --only-binary :all: backports.zoneinfo psycopg2-binary -r requirements.txt
+pip install -r requirements.txt
 
 # Run migrations
 python manage.py migrate
