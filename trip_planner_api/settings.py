fa552fc28c555801e6765d379d805a3703f2f6a9
mohamed belhadj <belmoh70@gmail.com>
2025-04-05 23:57:32 +0200

    front-url

--- trip_planner_api/settings.py
+++ trip_planner_api/settings.py
@@ -54,9 +54,31 @@ MIDDLEWARE = [
 # FRONTEND INTEGRATION
 # ========================
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
     "https://trip-planner-front-five.vercel.app", 
+    "http://localhost:3000",
 ]
+
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
+
 
 CSRF_TRUSTED_ORIGINS = CORS_ALLOWED_ORIGINS.copy()
 
