70a1d5ec32c6671b9f70d7d77ee28ad5c1f69a2d
mohamed belhadj <belmoh70@gmail.com>
2025-04-06 10:41:18 +0200

    fix settings auth not allowed

--- trip_planner_api/settings.py
+++ trip_planner_api/settings.py
@@ -42,6 +42,10 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware'
 ]
 
 # 'django.middleware.security.SecurityMiddleware',
